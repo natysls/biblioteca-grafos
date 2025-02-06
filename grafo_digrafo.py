1e12319ed0847f545312be689b4759a6cdd7b9bd
Natalia <natalia.sales@uece.br>
2025-02-06 19:38:57 -0300

    matriz de adjacencia implementada como um dicionario

--- grafo_digrafo.py
+++ grafo_digrafo.py
@@ -1,24 +1,17 @@
+from collections import defaultdict
 class Grafo:
     def __init__(self, direcionado=False, usar_matriz=False):
         self.direcionado = direcionado
         self.usar_matriz = usar_matriz
         self.vertices = set()
-        self.arestas = []
         self.pesos = {} if not usar_matriz else None
         self.lista_adjacencia = {} if not usar_matriz else None
-        self.matriz_adjacencia = [] if usar_matriz else None
+        self.matriz_adjacencia = defaultdict(dict) if usar_matriz else None
     
     def adicionar_aresta(self, u, v, peso=1):
         self.vertices.update([u, v])
-        self.arestas.append((u, v))
-        
+
         if self.usar_matriz:
-            max_index = max(u, v)
-            while len(self.matriz_adjacencia) <= max_index:
-                self.matriz_adjacencia.append([0] * (max_index + 1))
-                for linha in self.matriz_adjacencia:
-                    linha.extend([0] * (max_index + 1 - len(linha)))
-            
             self.matriz_adjacencia[u][v] = peso
 
             if not self.direcionado:
@@ -37,15 +30,8 @@ class Grafo:
                 self.lista_adjacencia[v].append(u)
                 self.pesos[(v, u)] = peso
 
-    def n(self):
+    def n(self): # Vértices
         return len(self.vertices)
-
-    def m(self):
-        if self.usar_matriz:
-            return sum(row.count(1) for row in self.matriz) // (1 if self.direcionado else 2)
-        return len(self.pesos) // (1 if self.direcionado else 2)
-
-
 
 class Digrafo(Grafo):
     def __init__(self, usar_matriz=False):
